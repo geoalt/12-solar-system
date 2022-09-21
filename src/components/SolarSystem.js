9a58e8f74afb5825652fab29a4ca8ec03d5fb39e
Diogo Martins <me.martinsfe@gmail.com>
2022-09-21 16:12:37 -0300

    feat: render planetcard in the app

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import '../App.css';
+import planets from '../data/planets';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+
+        <div className="planets-container">
+          {planets.map(({ name, image }, index) => (<PlanetCard
+            key={ index }
+            planetName={ name }
+            planetImage={ image }
+          />))}
+        </div>
+
       </div>
     );
   }
