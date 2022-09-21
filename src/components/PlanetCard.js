979963d6b42ea621c6f316bbcc65066144d3f8d2
Diogo Martins <me.martinsfe@gmail.com>
2022-09-21 16:11:45 -0300

    feat: add planetcard component

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card" className="planet-card">
+        <p data-testid="planet-name" className="planet-card__name">{planetName}</p>
+        <img
+          src={ planetImage }
+          alt={ `Planeta ${planetName}` }
+          className="planet-card__image"
+        />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
+
+export default PlanetCard;
